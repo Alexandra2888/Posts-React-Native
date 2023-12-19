8f6ea392387ca86eb9ca56c377b711cfecd57d04
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-12-19 13:09:48 +0200

    revert tabBarOptions

--- navigator/index.tsx
+++ navigator/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { CustomTabBar } from '../components/common';
-import { StackInTabInMain, StackInPostDetail } from '../screens';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {CustomTabBar} from '../components/common';
+import {
+  StackInTabInMain,
+  StackInPostDetail,
+} from '../screens';
 
 const Stack = createStackNavigator();
+
 const Tab = createBottomTabNavigator();
 
 const MainTabNavigator = () => {
@@ -13,11 +17,20 @@ const MainTabNavigator = () => {
     <Tab.Navigator
       initialRouteName="MainTab"
       tabBar={props => <CustomTabBar {...props} />}
-    >
+      tabBarOptions={{
+        style: {
+          backgroundColor: 'transparent',
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          bottom: 0,
+          elevation: 0,
+        },
+      }}>
       <Tab.Screen
         name="MainTab"
         component={StackInTabInMain}
-        options={{ tabBarLabel: 'POSTS' }}
+        options={{tabBarLabel: 'POSTS', title: 'Posts'}}
       />
     </Tab.Navigator>
   );
@@ -26,15 +39,19 @@ const MainTabNavigator = () => {
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="MainStack" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="MainStack">
         <Stack.Screen
           name="MainStack"
           component={MainTabNavigator}
+          options={{
+            headerShown: false,
+            title: 'Back',
+          }}
         />
         <Stack.Screen
           name="PostDetailStack"
           component={StackInPostDetail}
-          options={{ title: 'Post Detail' }}
+          options={{title: 'Post Detail'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
